c0bc6e7549038e145e4eb9f0996fa70ec6023c43
umarfarisi <pharese@gmail.com>
2017-11-23 20:39:57 +0700

    init commit

---
+++ src/main/java/com/akademik/mahasiswa/g4/AkademikMahasiswaApplication.java
@@ -0,0 +1,12 @@
+package com.akademik.mahasiswa.g4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkademikMahasiswaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkademikMahasiswaApplication.class, args);
+	}
+}
