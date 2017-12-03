3f208692255b8b30fdab64a8567784b6b014dd5d
umarfarisi <pharese@gmail.com>
2017-12-03 08:19:53 +0700

    fix bug in recognized beans class for mapper

--- src/main/java/com/akademik/mahasiswa/g4/AkademikMahasiswaApplication.java
+++ src/main/java/com/akademik/mahasiswa/g4/AkademikMahasiswaApplication.java
@@ -1,9 +1,11 @@
 package com.akademik.mahasiswa.g4;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.akademik.mahasiswa.g4.mapper")
 public class AkademikMahasiswaApplication {
 
 	public static void main(String[] args) {
