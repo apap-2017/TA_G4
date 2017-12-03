3a03743110fc2e1fb7dafb4fe2c4a9a2392b7ecd
umarfarisi <pharese@gmail.com>
2017-12-03 20:47:16 +0700

    add data to db and change some attribute

--- db/apap_akademik_mahasiswa.sql
+++ db/apap_akademik_mahasiswa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 02, 2017 at 09:57 AM
+-- Generation Time: Dec 03, 2017 at 02:46 PM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 7.1.7
 
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `kelas` (
   `id_riwayat_perkuliahan` int(11) NOT NULL,
-  `tanggal_pengisian` date NOT NULL,
+  `tanggal_pengisian` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `kode_mata_kuliah` varchar(50) NOT NULL,
   `kode_kurikulum` varchar(50) NOT NULL,
   `id_kelas` int(11) NOT NULL,
@@ -38,6 +38,47 @@ CREATE TABLE `kelas` (
   `nama_kelas` varchar(50) NOT NULL,
   `sks` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kelas`
+--
+
+INSERT INTO `kelas` (`id_riwayat_perkuliahan`, `tanggal_pengisian`, `kode_mata_kuliah`, `kode_kurikulum`, `id_kelas`, `nama_mata_kuliah`, `nama_kelas`, `sks`) VALUES
+(1, '2017-12-03 13:21:52', 'SI1', '2016.2017', 1, 'Bisnis Aja', 'BA1', 3),
+(2, '2017-12-03 13:21:52', 'SI1', '2016.2017', 1, 'Bisnis Aja', 'BA1', 3),
+(4, '2017-12-03 13:21:52', 'SI1', '2016.2017', 1, 'Bisnis Aja', 'BA1', 3),
+(6, '2017-12-03 13:21:52', 'SI1', '2016.2017', 1, 'Bisnis Aja', 'BA1', 3),
+(9, '2017-12-03 13:21:52', 'SI1', '2016.2017', 1, 'Bisnis Aja', 'BA1', 3),
+(1, '2017-12-03 13:21:52', 'SI1', '2016.2017', 2, 'Bisnis Itu', 'BI1', 4),
+(2, '2017-12-03 13:21:52', 'SI1', '2016.2017', 2, 'Bisnis Itu', 'BI1', 4),
+(4, '2017-12-03 13:21:52', 'SI1', '2016.2017', 2, 'Bisnis Itu', 'BI1', 4),
+(6, '2017-12-03 13:21:52', 'SI1', '2016.2017', 2, 'Bisnis Itu', 'BI1', 4),
+(9, '2017-12-03 13:21:52', 'SI1', '2016.2017', 2, 'Bisnis Itu', 'BI1', 4),
+(1, '2017-12-03 13:21:52', 'SI1', '2016.2017', 3, 'Bisnis Sana', 'BS1', 3),
+(3, '2017-12-03 13:21:52', 'SI1', '2016.2017', 3, 'Bisnis Sana', 'BS1', 3),
+(5, '2017-12-03 13:21:52', 'SI1', '2016.2017', 3, 'Bisnis Sana', 'BS1', 3),
+(7, '2017-12-03 13:21:52', 'SI1', '2016.2017', 3, 'Bisnis Sana', 'BS1', 3),
+(9, '2017-12-03 13:21:52', 'SI1', '2016.2017', 3, 'Bisnis Sana', 'BS1', 3),
+(1, '2017-12-03 13:21:52', 'SI1', '2016.2017', 4, 'Bisnis Lagi', 'BL1', 4),
+(3, '2017-12-03 13:21:52', 'SI1', '2016.2017', 4, 'Bisnis Lagi', 'BL1', 4),
+(5, '2017-12-03 13:21:52', 'SI1', '2016.2017', 4, 'Bisnis Lagi', 'BL1', 4),
+(7, '2017-12-03 13:21:52', 'SI1', '2016.2017', 4, 'Bisnis Lagi', 'BL1', 4),
+(10, '2017-12-03 13:21:52', 'SI1', '2016.2017', 4, 'Bisnis Lagi', 'BL1', 4),
+(2, '2017-12-03 13:21:52', 'SI1', '2016.2017', 5, 'Bisnis Admin', 'BA1', 3),
+(3, '2017-12-03 13:21:52', 'SI1', '2016.2017', 5, 'Bisnis Admin', 'BA1', 3),
+(5, '2017-12-03 13:21:52', 'SI1', '2016.2017', 5, 'Bisnis Admin', 'BA1', 3),
+(7, '2017-12-03 13:21:52', 'SI1', '2016.2017', 5, 'Bisnis Admin', 'BA1', 3),
+(10, '2017-12-03 13:21:52', 'SI1', '2016.2017', 5, 'Bisnis Admin', 'BA1', 3),
+(2, '2017-12-03 13:21:52', 'SI1', '2016.2017', 6, 'Coding Aja', 'CA1', 5),
+(4, '2017-12-03 13:21:52', 'SI1', '2016.2017', 6, 'Coding Aja', 'CA1', 5),
+(5, '2017-12-03 13:21:52', 'SI1', '2016.2017', 6, 'Coding Aja', 'CA1', 5),
+(8, '2017-12-03 13:21:52', 'SI1', '2016.2017', 6, 'Coding Aja', 'CA1', 5),
+(10, '2017-12-03 13:21:52', 'SI1', '2016.2017', 6, 'Coding Aja', 'CA1', 5),
+(2, '2017-12-03 13:21:52', 'SI1', '2016.2017', 7, 'Coding Bgt', 'CB1', 3),
+(4, '2017-12-03 13:21:52', 'SI1', '2016.2017', 7, 'Coding Bgt', 'CB1', 3),
+(6, '2017-12-03 13:21:52', 'SI1', '2016.2017', 7, 'Coding Bgt', 'CB1', 3),
+(8, '2017-12-03 13:21:52', 'SI1', '2016.2017', 7, 'Coding Bgt', 'CB1', 3),
+(10, '2017-12-03 13:21:52', 'SI1', '2016.2017', 7, 'Coding Bgt', 'CB1', 3);
 
 -- --------------------------------------------------------
 
@@ -55,6 +96,19 @@ CREATE TABLE `mahasiswa` (
   `angkatan` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`npm`, `nama`, `username`, `id_universitas`, `id_fakultas`, `id_prodi`, `angkatan`) VALUES
+('123456781', 'nama1', 'username1', 1, 1, 1, 2015),
+('123456782', 'nama2', 'username2', 1, 1, 1, 2015),
+('123456783', 'nama3', 'username3', 1, 1, 1, 2016),
+('123456784', 'nama4', 'username4', 1, 1, 1, 2018),
+('123456785', 'nama5', 'username5', 1, 1, 1, 2018),
+('123456786', 'nama6', 'username6', 1, 1, 1, 2018),
+('123456787', 'nama7', 'username7', 1, 1, 1, 2018);
+
 -- --------------------------------------------------------
 
 --
@@ -63,11 +117,27 @@ CREATE TABLE `mahasiswa` (
 
 CREATE TABLE `riwayat_perkuliahan` (
   `id` int(11) NOT NULL,
-  `status_irs` tinyint(1) NOT NULL,
+  `status_irs` tinyint(1) NOT NULL DEFAULT '0',
   `npm` char(9) NOT NULL,
   `tahun_ajar` varchar(50) NOT NULL,
   `term` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `riwayat_perkuliahan`
+--
+
+INSERT INTO `riwayat_perkuliahan` (`id`, `status_irs`, `npm`, `tahun_ajar`, `term`) VALUES
+(1, 0, '123456781', '2015-16', 1),
+(2, 0, '123456781', '2017-16', 2),
+(3, 0, '123456781', '2015-16', 2),
+(4, 0, '123456781', '2017-16', 1),
+(5, 0, '123456782', '2016-17', 1),
+(6, 0, '123456782', '2015-16', 2),
+(7, 0, '123456782', '2015-16', 1),
+(8, 0, '123456782', '2016-17', 2),
+(9, 0, '123456783', '2016-17', 1),
+(10, 0, '123456783', '2016-17', 2);
 
 -- --------------------------------------------------------
 
@@ -79,6 +149,22 @@ CREATE TABLE `user` (
   `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`username`, `password`) VALUES
+('username1', 'password1'),
+('username10', 'password10'),
+('username2', 'password2'),
+('username3', 'password3'),
+('username4', 'password4'),
+('username5', 'password5'),
+('username6', 'password6'),
+('username7', 'password7'),
+('username8', 'password8'),
+('username9', 'password9');
 
 -- --------------------------------------------------------
 
@@ -92,6 +178,22 @@ CREATE TABLE `user_role` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data for table `user_role`
+--
+
+INSERT INTO `user_role` (`username`, `role`) VALUES
+('username1', 'ROLE_MAHASISWA'),
+('username10', 'ROLE_SEKRETARIAT'),
+('username2', 'ROLE_MAHASISWA'),
+('username3', 'ROLE_MAHASISWA'),
+('username4', 'ROLE_MAHASISWA'),
+('username5', 'ROLE_MAHASISWA'),
+('username6', 'ROLE_MAHASISWA'),
+('username7', 'ROLE_MAHASISWA'),
+('username8', 'ROLE_SEKRETARIAT'),
+('username9', 'ROLE_SEKRETARIAT');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -99,7 +201,7 @@ CREATE TABLE `user_role` (
 -- Indexes for table `kelas`
 --
 ALTER TABLE `kelas`
-  ADD PRIMARY KEY (`id_kelas`),
+  ADD PRIMARY KEY (`id_kelas`,`id_riwayat_perkuliahan`),
   ADD KEY `id_riwayat_perkuliahan` (`id_riwayat_perkuliahan`);
 
 --
@@ -128,6 +230,15 @@ ALTER TABLE `user`
 ALTER TABLE `user_role`
   ADD PRIMARY KEY (`username`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `riwayat_perkuliahan`
+--
+ALTER TABLE `riwayat_perkuliahan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- Constraints for dumped tables
 --
