42bd839672f14e5f80f9740969e061fbfce1a0c0
umarfarisi <pharese@gmail.com>
2017-12-04 14:59:36 +0700

    connect lihar-irs with IRSModel

--- src/main/java/com/akademik/mahasiswa/g4/controller/IRSController.java
+++ src/main/java/com/akademik/mahasiswa/g4/controller/IRSController.java
@@ -1,19 +1,15 @@
 package com.akademik.mahasiswa.g4.controller;
 
-import com.akademik.mahasiswa.g4.model.IRSFormModel;
 import com.akademik.mahasiswa.g4.model.rest.*;
 import com.akademik.mahasiswa.g4.service.IRSService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 @Controller
--- src/main/java/com/akademik/mahasiswa/g4/model/view/IRSModel.java
+++ src/main/java/com/akademik/mahasiswa/g4/model/view/IRSModel.java
@@ -2,6 +2,7 @@ package com.akademik.mahasiswa.g4.model.view;
 
 import com.akademik.mahasiswa.g4.model.db.MahasiswaDBModel;
 import com.akademik.mahasiswa.g4.model.db.RiwayatPerkuliahanModel;
+import com.akademik.mahasiswa.g4.model.rest.TermModel;
 import lombok.*;
 
 import java.util.List;
@@ -18,5 +19,8 @@ public class IRSModel {
     private List<String> error;
     private String sksMaksimum;
     private String ipTerakhir;
+
+    //drive atribut
+    private int totalSks;
 
 }
