12ecc6e63b8216a81e0269496d29b660b611a4c1
umarfarisi <pharese@gmail.com>
2017-12-05 18:00:55 +0700

    create api json parser for getPrasyarat

--- src/main/java/com/akademik/mahasiswa/g4/dao/KurikulumDAO.java
+++ src/main/java/com/akademik/mahasiswa/g4/dao/KurikulumDAO.java
@@ -2,10 +2,15 @@ package com.akademik.mahasiswa.g4.dao;
 
 import com.akademik.mahasiswa.g4.model.rest.KurikulumModel;
 import com.akademik.mahasiswa.g4.model.rest.KurikulumResponseModel;
+import com.akademik.mahasiswa.g4.model.rest.MatakuliahModel;
+import com.akademik.mahasiswa.g4.model.rest.PrasyaratMatkulResponseModel;
 import com.akademik.mahasiswa.g4.utls.NetworkUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class KurikulumDAO {
@@ -21,6 +26,19 @@ public class KurikulumDAO {
             return response.getResult();
         }
         return null;
+    }
+
+    public List<MatakuliahModel> getPrasyaratMatkul(String kodeMatkul, String kodeKurikulum){
+
+        PrasyaratMatkulResponseModel response = restTemplateBuilder
+                .build().getForObject(NetworkUtils.BASE_URL_KURIKULUM + "/api/getPrasyarat/" + kodeMatkul + "/" + kodeKurikulum
+                        ,PrasyaratMatkulResponseModel.class);
+
+        if(response.getStatus() == 200){
+            return response.getResult().getMatkulPrasyarats();
+        }
+
+        return new ArrayList<>();
     }
 
 }
